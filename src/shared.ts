6b5314533791e01fd637a8319356ab19661a927f
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-11-28 17:53:17 +0100

    adjusted shared.ts

--- src/shared.ts
+++ src/shared.ts
@@ -1,5 +1,3 @@
-import { JSDOM } from 'jsdom'
-
 export interface Tour_Result {
   DVV_ID: number
   Place: number
@@ -27,32 +25,4 @@ export enum Tables {
   Players = 'Players',
   Teams = 'Teams',
   Tour_Result = 'Tour_Result'
-}
-
-export function createDOM(body: string) {
-  return new JSDOM(body).window.document
-}
-
-/**
- * Holds constructed URLs of scraping scenarios
- */
-export class DvvURLs {
-  private static base = process.env.BEACH_URL
-
-  /**
-   * Returns https://beach.volleyball-verband.de/public/spieler.php?id={id} with given id as query parameter
-   *
-   * @id number
-   */
-  static Player(id: number) {
-    return `${DvvURLs.base}/spieler.php?id=${id}`
-  }
-
-  static Team(id: number) {
-    return `${DvvURLs.base}/team.php?id=${id}`
-  }
-
-  static Tour_Result(id: number) {
-    return `${DvvURLs.base}/tur-er.php?id=${id}`
-  }
 }
